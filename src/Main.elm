0d9f65e655861075bbe4febb91faf0229ce4920e
wasuken <wevorence@gmail.com>
2020-04-05 22:30:52 +0900

    impl paginator.

--- src/Main.elm
+++ src/Main.elm
@@ -6,6 +6,8 @@ import Bootstrap.CDN as CDN
 import Bootstrap.Card as Card
 import Bootstrap.Card.Block as CBlock
 import Bootstrap.Form.Input as Input
+import Bootstrap.Navbar as Navbar
+import Bootstrap.Pagination as Pagination
 import Bootstrap.Utilities.Display as Display
 import Bootstrap.Utilities.Size as Size
 import Browser
@@ -15,8 +17,9 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http exposing (Error)
 import Json.Decode as Json
+import List.Extra exposing (..)
 import Task exposing (Task, andThen, sequence, succeed)
-import Time exposing (Posix, Month(..), millisToPosix, utc, now, posixToMillis)
+import Time exposing (Month(..), Posix, millisToPosix, now, posixToMillis, utc)
 import Time.Extra as Time
 
 
@@ -47,18 +50,33 @@ type alias Story =
     }
 
 
+type StoryType
+    = New
+    | Ask
+    | Top
+    | Job
+    | Show
+
+
 type alias Model =
     { stories : List Story
-    , idList : List Int
+    , idList : List (List Int)
     , dlStatus : DownloadStatus
     , maxStoriesNum : Int
     , nowDatePosix : Maybe Posix
+    , storyType : StoryType
+    , navbarState : Navbar.State
+    , offset : Int
     }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model [] [] Loading 20 Nothing, Cmd.batch [ getStoryIdList topStoryIdListUrl, setToday ] )
+    let
+        ( navbarState, navCmd ) =
+            Navbar.initialState NavbarMsg
+    in
+    ( Model [] [ [] ] Loading 20 Nothing Top navbarState 0, Cmd.batch [ getStoryIdList (storyIdListUrl Top), setToday, navCmd ] )
 
 
 
@@ -75,6 +93,9 @@ type Msg
     = GotIdList (Result Http.Error (List Int))
     | GotStoryInfo (Result Http.Error Story)
     | ReceivePosixDate Posix
+    | NavbarMsg Navbar.State
+    | ChangeStoryType StoryType
+    | PaginationMsg Int
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -84,11 +105,22 @@ update msg model =
             case result of
                 Ok idList ->
                     let
+                        idList_ =
+                            groupsOf model.maxStoriesNum idList
+
+                        ids =
+                            case List.head (List.drop model.offset idList_) of
+                                Just lst ->
+                                    lst
+
+                                Nothing ->
+                                    []
+
                         c =
-                            Cmd.batch (List.map (\id -> Http.get { url = getStoryInfoUrl (String.fromInt id), expect = Http.expectJson GotStoryInfo storyDecoder }) (List.take model.maxStoriesNum idList))
+                            Cmd.batch (storiesASync ids)
                     in
                     ( { model
-                        | idList = idList
+                        | idList = idList_
                         , dlStatus = Success
                         , stories = []
                       }
@@ -116,25 +148,36 @@ update msg model =
             , Cmd.none
             )
 
+        NavbarMsg state ->
+            ( { model | navbarState = state }, Cmd.none )
+
+        ChangeStoryType t ->
+            let
+                cmd =
+                    getStoryIdList (storyIdListUrl t)
+            in
+            ( { model | storyType = t}, cmd )
+
+        PaginationMsg index ->
+            let
+                ids =
+                    case List.head (List.drop model.offset model.idList) of
+                        Just lst ->
+                            lst
+
+                        Nothing ->
+                            []
+
+                cmd =
+                    Cmd.batch (storiesASync ids)
+            in
+            ( { model | offset = index
+                      , stories = [] }, cmd )
+
 
 setToday : Cmd Msg
 setToday =
     now |> Task.perform ReceivePosixDate
-
-
-viewIdList : Model -> Html Msg
-viewIdList model =
-    case model.dlStatus of
-        Failure ->
-            div []
-                [ text "Failed connect server." ]
-
-        Loading ->
-            text "Loading..."
-
-        Success ->
-            div []
-                [ ul [] (List.map (\x -> li [] [ text (String.fromInt x) ]) model.idList) ]
 
 
 viewHNCard : Story -> Posix -> Html Msg
@@ -159,8 +202,11 @@ viewHNCard s now =
 formatHourDiff : Int -> Posix -> String
 formatHourDiff targetSec now =
     let
-        nowSec = posixToMillis now // 1000
-        df = (nowSec - targetSec) // (60 * 60)
+        nowSec =
+            posixToMillis now // 1000
+
+        df =
+            (nowSec - targetSec) // (60 * 60)
     in
     String.fromInt df
 
@@ -186,19 +232,72 @@ viewStories model =
 
         Success ->
             div []
-                [ ul [] (List.map (\x -> viewHNCard x now) model.stories) ]
+                [ Navbar.config NavbarMsg
+                    |> Navbar.withAnimation
+                    |> Navbar.brand [ href "#" ] [ text "Brand" ]
+                    |> Navbar.items
+                        [ Navbar.itemLink [ href "#", onClick (ChangeStoryType Top) ] [ text "Top" ]
+                        , Navbar.itemLink [ href "#", onClick (ChangeStoryType New) ] [ text "New" ]
+
+                        -- , Navbar.itemLink [ href "#", onClick (ChangeStoryType Ask) ] [ text "Ask" ]
+                        , Navbar.itemLink [ href "#", onClick (ChangeStoryType Job) ] [ text "Job" ]
+                        , Navbar.itemLink [ href "#", onClick (ChangeStoryType Show) ] [ text "Show" ]
+                        ]
+                    |> Navbar.view model.navbarState
+                , ul [] (List.map (\x -> viewHNCard x now) model.stories)
+                , Pagination.defaultConfig
+                    |> Pagination.ariaLabel "Pagination"
+                    |> Pagination.large
+                    |> Pagination.itemsList
+                        { selectedMsg = PaginationMsg
+                        , prevItem = Just <| Pagination.ListItem [] [ text "Previous" ]
+                        , nextItem = Just <| Pagination.ListItem [] [ text "Next" ]
+                        , activeIdx = model.offset
+                        , data = model.stories
+                        , itemFn = \idx _ -> Pagination.ListItem [] [ text <| String.fromInt (idx + 1) ]
+                        , urlFn = \idx _ -> "#/pages/" ++ String.fromInt (idx + 1)
+                        }
+                    |> Pagination.view
+                ]
 
 
 
--- viewStories : Model -> Html Msg
--- viewStories model ->
---             case model
 -- HTTP
+
+
+storiesASync : List Int -> List (Cmd Msg)
+storiesASync ids =
+    List.map (\id -> Http.get { url = getStoryInfoUrl (String.fromInt id), expect = Http.expectJson GotStoryInfo storyDecoder }) ids
+
+
+createStoryIdListUrl : String -> String
+createStoryIdListUrl name =
+    "https://hacker-news.firebaseio.com/v0/" ++ name ++ ".json"
 
 
 topStoryIdListUrl : String
 topStoryIdListUrl =
-    "https://hacker-news.firebaseio.com/v0/topstories.json"
+    createStoryIdListUrl "topstories"
+
+
+newStoryIdListUrl : String
+newStoryIdListUrl =
+    createStoryIdListUrl "newstories"
+
+
+showStoryIdListUrl : String
+showStoryIdListUrl =
+    createStoryIdListUrl "showstories"
+
+
+askStoryIdListUrl : String
+askStoryIdListUrl =
+    createStoryIdListUrl "askstories"
+
+
+jobStoryIdListUrl : String
+jobStoryIdListUrl =
+    createStoryIdListUrl "jobstories"
 
 
 getStoryInfoUrl : String -> String
@@ -212,6 +311,25 @@ getStoryIdList url =
         { url = url
         , expect = Http.expectJson GotIdList (Json.list Json.int)
         }
+
+
+storyIdListUrl : StoryType -> String
+storyIdListUrl t =
+    case t of
+        Job ->
+            jobStoryIdListUrl
+
+        Show ->
+            showStoryIdListUrl
+
+        Ask ->
+            askStoryIdListUrl
+
+        Top ->
+            topStoryIdListUrl
+
+        New ->
+            newStoryIdListUrl
 
 
 storyDecoder : Json.Decoder Story
